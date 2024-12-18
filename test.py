5544dc028948a76a8a0e12bca26838d2a1e373cc
Jeffery <nkes244@gmail.com>
2024-12-18 17:33:09 +0800

    message

--- test.py
+++ test.py
@@ -2,9 +2,11 @@ import time
 import qrcode
 import os
 import random
-while True:
-    number=random.random()
-    img = qrcode.make(str(number))    # 要轉換成 QRCode 的文字
-    img.save('qrcode.png') 
-    time.sleep(15)
-    os.remove("qrcode.png")
+def generate_qrcode():
+    while True:
+        number=random.random()
+        img = qrcode.make(str(number))    # 將隨機數轉換成QRCode
+        img.save('qrcode.png') 
+        time.sleep(15)
+        os.remove("qrcode.png")
+generate_qrcode()
